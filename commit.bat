ac2f397de19a4a7d1fbc93d957e1f20226b92e68
Daniel Nichols <thebagmaster@gmail.com>
2014-09-29 11:27:03 -0500

    Added Screenshot and Batch

---
+++ commit.bat
@@ -0,0 +1,7 @@
+git.exe add .
+git.exe commit -m "Added Screenshot and Batch"
+git.exe remote add origin https://github.com/thebagmaster/CPP_ARAB_League_of_Legends.git
+git.exe pull https://github.com/thebagmaster/CPP_ARAB_League_of_Legends.git
+git.exe push origin master
+echo Press Enter...
+read
